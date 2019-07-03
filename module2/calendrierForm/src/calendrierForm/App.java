1e8087b95566a52f28014fba1de5acc80adf136a
vincent courtalon <vincent.courtalon@gmail.com>
2019-07-03 17:09:30 +0200

    fin calendrier

--- module2/calendrierForm/src/calendrierForm/App.java
+++ module2/calendrierForm/src/calendrierForm/App.java
@@ -16,7 +16,45 @@ public class App {
 	
 	public static void calendrier(int annee, int mois) {
 		calendrier_header(annee, mois);
-		//System.out.println(nbJoursMois(annee, mois));
+		calendrier_body(annee, mois);
+	}
+	public static void calendrier_body(int annee, int mois) {
+		int jourdebut = jourDebut(annee, mois);
+		int nbjoursmois = nbJoursMois(annee, mois);
+		
+		// boucle d'affichage
+		// decalage en fonction du jour de semaine de début
+		for (int marge = 0; marge < jourdebut; marge++)
+			System.out.print("    ");
+		
+		for (int jours = 0; jours < nbjoursmois; jours++) {
+			if (jours < 9)
+				System.out.print(" "); // si le no jour fait 1 caractere, ajouter un espace pour alignement
+			System.out.print(" " + (jours + 1) + " ");
+			if ((jours + jourdebut) % 7 == 6) // si on est un dimanche, passer a la ligne
+				System.out.println();
+		}
+		
+				
+	}
+	
+	public static int jourDebut(int annee, int mois) {
+		int nbjours = 2; // on demarre un mercredi le 1er janvier 1800
+		
+		// année par année (boucle)
+		for (int a = 1800; a < annee; a++) {
+			if (estBissextile(a))
+				nbjours += 366;
+			else
+				nbjours += 365;
+		}
+		// je suis sur le 1er janvier de l'année qui m'intéresse
+		for (int m = 1; m < mois; m++) {
+			nbjours += nbJoursMois(annee, m);
+		}
+		// je suis sur le bon jour, renvoyer quelle jour de la semaine on est
+		
+		return nbjours % 7;
 	}
 	
 	public static void calendrier_header(int annee, int mois) {
