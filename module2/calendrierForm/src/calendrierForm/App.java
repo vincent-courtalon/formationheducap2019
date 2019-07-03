d07bdb0d99e7731ad030cd4d3fc93e39ad010d42
vincent courtalon <vincent.courtalon@gmail.com>
2019-07-03 15:49:58 +0200

    deuxieme etape calendrier

--- module2/calendrierForm/src/calendrierForm/App.java
+++ module2/calendrierForm/src/calendrierForm/App.java
@@ -15,14 +15,61 @@ public class App {
 	}
 	
 	public static void calendrier(int annee, int mois) {
-		
+		calendrier_header(annee, mois);
+		//System.out.println(nbJoursMois(annee, mois));
 	}
 	
 	public static void calendrier_header(int annee, int mois) {
-		
+		System.out.println("---------------------------");
+		System.out.println("    "+  nomMois(mois) + " " + annee);
+		System.out.println("LUN MAR MER JEU VEN SAM DIM");
 	}
 	
 	public static String nomMois(int mois) {
-		return "";
+		switch(mois) {
+			case 1: return "janvier";
+			case 2: return "fevrier";
+			case 3: return "mars";
+			case 4: return "avril";
+			case 5: return "mai";
+			case 6: return "juin";
+			case 7: return "juillet";
+			case 8: return "aout";
+			case 9: return "septembre";
+			case 10: return "octobre";
+			case 11: return "novembre";
+			case 12: return "decembre";
+			default: return "";
+		}
+		
 	}
+	
+	
+	
+	public static int nbJoursMois(int annee, int mois) {
+		switch(mois) {
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12: return 31;
+			case 4:
+			case 6:
+			case 9:
+			case 11: return 30;
+			case 2:
+				return (estBissextile(annee))? 29 : 28;
+		}
+		return 0;
+	}
+	
+	public static boolean estBissextile(int annee) {
+		//       tous les 4 ans masi pas les siecles  OU tous les 400 ans 
+		return (annee % 4 == 0 && annee % 100 != 0) || (annee % 400 == 0);
+	}
+	
+	
+	
 }
