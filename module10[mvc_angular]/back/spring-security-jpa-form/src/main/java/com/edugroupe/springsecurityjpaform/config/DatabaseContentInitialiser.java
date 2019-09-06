87e5477a9b5e9b4f5862425524cc5c7bc8b964df
Stagiaire <Stagiaire@INSTRUCT>
2019-09-06 11:56:59 +0200

    ajout du hashage des mots de passe

---
+++ module10[mvc_angular]/back/spring-security-jpa-form/src/main/java/com/edugroupe/springsecurityjpaform/config/DatabaseContentInitialiser.java
@@ -0,0 +1,67 @@
+package com.edugroupe.springsecurityjpaform.config;
+
+import java.util.HashSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.edugroupe.springsecurityjpaform.metier.Role;
+import com.edugroupe.springsecurityjpaform.metier.User;
+import com.edugroupe.springsecurityjpaform.repositories.RoleRespository;
+import com.edugroupe.springsecurityjpaform.repositories.UserRepository;
+
+
+/*
+ *  cette classe ecoute le ContextRefreshedEvent
+ *  dans notre cas, cela sera déclenché après initalisation de tous les bean spring
+ *  mais avant que le travail de l'application ne démarre réellement
+ *  
+ *  donc:
+ *  	- tous les bean spring sont disponnible et pret à l'emploie
+ *   		y compris les repositories
+ *   	- mais aucun traitement de l'application normal (requette http, controller)
+ *   		n'ont envore été éxécuté
+ *  spring reconnaitra le fait qu'on implement l'interface ApplicationListener
+ *  par contre, il faut bien déclarer notre classe comme service ou component
+ *   si on veut qu'elle soit initialisée dans notre application
+ */
+@Service
+public class DatabaseContentInitialiser implements ApplicationListener<ContextRefreshedEvent> {
+
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private RoleRespository roleRepository;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		if (userRepository.count() == 0) {
+			// il n'existe pas d'utilisateur dans la base
+			// a nous de jouer!
+			System.out.println("la base est vide, creation des user et role par defaut");
+			Role r_admin = roleRepository.save(new Role(0, "ROLE_ADMIN"));
+			Role r_user = roleRepository.save(new Role(0, "ROLE_USER"));
+			
+			User admin = new User(0, "admin", passwordEncoder.encode("admin"), true);
+			admin.setRoles(new HashSet<>());
+			admin.getRoles().add(r_admin);
+			admin.getRoles().add(r_user);
+			userRepository.save(admin);
+			
+			User vincent = new User(0, "vincent", passwordEncoder.encode("1234"), true);
+			vincent.setRoles(new HashSet<>());
+			vincent.getRoles().add(r_user);
+			userRepository.save(vincent);
+		}
+		else
+			System.out.println("la base contient déjà des utilisateurs");
+		
+	}
+
+	
+}
--- module10[mvc_angular]/back/spring-security-jpa-form/src/main/java/com/edugroupe/springsecurityjpaform/config/SecurityConfig.java
+++ module10[mvc_angular]/back/spring-security-jpa-form/src/main/java/com/edugroupe/springsecurityjpaform/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -17,7 +18,11 @@ import com.edugroupe.springsecurityjpaform.security.MyUserDetailsService;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
-	public PasswordEncoder getPasswordEncoder() { return NoOpPasswordEncoder.getInstance(); }
+	public PasswordEncoder getPasswordEncoder() { 
+		//return NoOpPasswordEncoder.getInstance();
+		return new  BCryptPasswordEncoder();
+		
+	}
 
 	@Autowired
 	private MyUserDetailsService myUserDetailsService;
